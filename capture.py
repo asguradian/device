67daacd52aa73e324b5be4c1c1fdc203e34c1ac9
Anil <anil.acharya6851@gmail.com>
2019-06-25 21:14:53 -0400

    the script to provided unique filename to each file added to the directory

--- capture.py
+++ capture.py
@@ -1,10 +1,11 @@
 import time
 import cv2
 import argparse
+from multiprocessing import Queue
 #video = cv2.VideoCapture(0);
-
-
-# This script captures the image at the defined frame rate and resize the image to
+import os
+queue=Queue() #holds previously used fileName 
+# # This script captures the image at the defined frame rate and resize the image to
 # to the defined dimension and store it to the specified directory. The files are stored in a circular 
 # manner such that at every point of time there is a fixed number of image on the directory. It deletes the old images to make roon for the new images.
 counter=0;
@@ -41,9 +42,14 @@ while(cap.isOpened()):
  x = float(w/img.shape[1])
  y = float(h/img.shape[0])
  resized= cv2.resize(img, (0, 0), fx=x, fy=y)
- if(counter>f):
-  counter=counter % f;
- cv2.imwrite(o+"/img_"+str(counter % f)+".png",resized)
+ if(counter>f-1):
+   oldestImage=queue.get(block=False);
+   if os.path.exists(oldestImage):
+     os.remove(oldestImage)
+ newImage= o+"/"+str(counter)+".png";  
+ queue.put(newImage)
+ cv2.imwrite(newImage,resized)
  counter+=1
- time.sleep(timeSleep) 
+ time.sleep(timeSleep)
+
 
